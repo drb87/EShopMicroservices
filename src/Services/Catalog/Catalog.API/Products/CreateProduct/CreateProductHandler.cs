5fd15f39e24927774b098ba21e765f957891fa06
Diego Rodriguez <diego.r.baru@gmail.com>
2024-09-04 18:46:50 -0300

    catalog initial

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+: ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler(IDocumentSession session) 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken token)
+    {
+        // create Product entity from command object
+        var product = new Product
+        {
+            Category = command.Category,
+            Name = command.Name,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // save to database
+        session.Store(product);
+        await session.SaveChangesAsync(token);
+
+        // return CreateProductResult result
+        return new CreateProductResult(product.Id);
+    }
+}
